22548227a4232af7fa31b41306495d7ed3155afd
Ccino Lee <ccinodev@gmail.com>
2020-05-31 21:18:59 +0900

    Updates

---
+++ precache-manifest.e30a168d84cd26e39575fe118362425f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8223fd7fa01b78a84d4ecb7958d729ce",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "330568e166a0e6bcd593",
+    "url": "/movie_app/static/css/main.f22166bb.chunk.css"
+  },
+  {
+    "revision": "98be54eaa9ed6a5a4f95",
+    "url": "/movie_app/static/js/2.cfee456d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.cfee456d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "330568e166a0e6bcd593",
+    "url": "/movie_app/static/js/main.e862443a.chunk.js"
+  },
+  {
+    "revision": "4dcb005dec2f66fe5231",
+    "url": "/movie_app/static/js/runtime-main.615d8c0e.js"
+  }
+]);
